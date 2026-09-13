f1c22e77e4e71e52582a1bb3925501fcbb57db6d
mo-durazi <mo.durazi.106@gmail.com>
2026-09-14 00:25:17 +0300

    added the initial campaign Manger model

--- models/campaignManager.js
+++ models/campaignManager.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const campaignManagerSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    unique: true
-  }
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const CampaignManager = mongoose.model('CampaignManager', campaignManagerSchema);
