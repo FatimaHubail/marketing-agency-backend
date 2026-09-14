ede788e2e0e325e3ef6d11f6ef6830e8d3f10c58
MuneeraAlmajed <muneeraalmajed00@gmail.com>
2026-09-14 14:59:28 +0300

    Finished Agency Backend

--- routes/agencyClientRoutes.js
+++ routes/agencyClientRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const agencyClientCtrl = require('../controllers/agencyClientCtrl');
 
 router.get('/',agencyClientCtrl.getClients);
+router.get('/:id',agencyClientCtrl.getClient);
+router.delete('/:id',agencyClientCtrl.deleteClient);
 
 module.exports = router;
