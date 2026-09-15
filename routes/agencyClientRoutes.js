7b983611f90e28bcf9868a6321c988383a09fb72
mo-durazi <mo.durazi.106@gmail.com>
2026-09-15 23:40:34 +0300

    Secure client routes with isStaff and isAdmin middlewares

--- routes/agencyClientRoutes.js
+++ routes/agencyClientRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const isClient = require('../middleware/isClient');
+const isStaff = require('../middleware/isStaff');
+const isAdmin = require('../middleware/isAdmin');
 const agencyClientCtrl = require('../controllers/agencyClientCtrl');
 
-router.get('/',agencyClientCtrl.getClients);
+router.get('/', isStaff, agencyClientCtrl.getClients);
 router.get('/:id', isClient, agencyClientCtrl.getClient);
 router.put('/:id', isClient, agencyClientCtrl.updateClient);
-router.delete('/:id',agencyClientCtrl.deleteClient);
+router.delete('/:id', isAdmin, agencyClientCtrl.deleteClient);
 
 module.exports = router;
