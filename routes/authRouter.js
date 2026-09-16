4f5fa1a38e69a75be61ea1911f04d72541f170ab
FatimaHubail <fhubail03@gmail.com>
2026-09-16 10:01:11 +0300

    Remove signup route and related logic from auth controller

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,7 +3,6 @@ const authCtrl = require('../controllers/authCtrl');
 
 const router = express.Router();
 
-router.post('/sign-up', authCtrl.signup);
 router.post('/sign-in', authCtrl.login);
 router.post('/register', authCtrl.registerClient);
 
