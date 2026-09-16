826a322f7a782df0785e283c2add66df5203b8f6
mo-durazi <mo.durazi.106@gmail.com>
2026-09-16 06:43:53 +0300

    another conflict

--- routes/outsource/outsourceTaskRoutes.js
+++ routes/outsource/outsourceTaskRoutes.js
@@ -11,6 +11,6 @@ router.get('/', isOutsource, outsourceTaskCtrl.getMyOutsourceTasks);
 router.get('/all', isStaff, outsourceTaskCtrl.index);
 router.get('/:id', isTaskOwner(), outsourceTaskCtrl.show);
 router.put('/:id', isTaskOwner(), outsourceTaskCtrl.update);
-router.delete('/:id', isStaff, outsourceTaskCtrl.deleteOutsourceTask);
+router.delete('/:id', isStaff, outsourceTaskCtrl.delete);
 
 module.exports = router;
